3c3e51d32110839c5dd68a748ea615e8d076f462
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-09-23 15:53:49 +0530

    flatmates bill sharing app - planning

---
+++ Flatmates_Bill/main.py
@@ -0,0 +1,44 @@
+class Bill:
+    """
+    Object that contains data about a bill, such as
+    total amount and period of the bill.
+    """
+
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+
+
+class Flatmate:
+    """
+    Creates a flatmate person who lives in the flat
+    and pays a share of the bill.
+    """
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        return bill.amount / 2
+
+
+class PdfReport:
+    """
+    Creates a pdf file that contains data about
+    the flatmates such as their names, their due
+    amounts and the period of the bill.
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
+
+
+the_bill = Bill(amount=120, period="March 2023")
+john = Flatmate(name="John", days_in_house=20)
+marry = Flatmate(name="Marry", days_in_house=25)
+
+print(john.pays(bill=the_bill))
