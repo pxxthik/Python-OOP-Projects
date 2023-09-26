e4a91064d892ab4c4d707c6416729f1b2c06f11e
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-09-26 16:12:19 +0530

    pays method of a flatmate class

--- Flatmates_Bill/main.py
+++ Flatmates_Bill/main.py
@@ -19,8 +19,10 @@ class Flatmate:
         self.name = name
         self.days_in_house = days_in_house
 
-    def pays(self, bill):
-        return bill.amount / 2
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * weight
+        return to_pay
 
 
 class PdfReport:
@@ -41,4 +43,5 @@ the_bill = Bill(amount=120, period="March 2023")
 john = Flatmate(name="John", days_in_house=20)
 marry = Flatmate(name="Marry", days_in_house=25)
 
-print(john.pays(bill=the_bill))
+print("John pays: ", john.pays(bill=the_bill, flatmate2=marry))
+print("Marry pays: ", marry.pays(bill=the_bill, flatmate2=john))
