c39ab76a12b3da41a1bd5afdabaf7d5d44a3e292
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-05 11:10:13 +0530

    implement image search functionality to photo searcher app

--- Photo_Searcher/main.py
+++ Photo_Searcher/main.py
@@ -2,12 +2,29 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
+import wikipedia
+import requests
+
 Builder.load_file('frontend.kv')
 
 
 class FirstScreen(Screen):
     def search_image(self):
-        pass
+        # Get user query from text input
+        query = self.manager.current_screen.ids.user_query.text
+
+        # Get wikipedia page and first image link
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+
+        # Download the image
+        req = requests.get(image_link)
+        image_path = f"files/{query}.jpg"
+        with open(image_path, "wb") as file:
+            file.write(req.content)
+
+        # Set the image in the widget
+        self.manager.current_screen.ids.img.source = image_path
 
 
 class RootWidget(ScreenManager):
