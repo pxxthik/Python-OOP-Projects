8c6be7227fc75bcc6d8814102809cbe3c56f90f0
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-05 11:26:57 +0530

    arrange code of photo searcher app

--- Photo_Searcher/main.py
+++ Photo_Searcher/main.py
@@ -9,7 +9,7 @@ Builder.load_file('frontend.kv')
 
 
 class FirstScreen(Screen):
-    def search_image(self):
+    def get_image_link(self):
         # Get user query from text input
         query = self.manager.current_screen.ids.user_query.text
 
@@ -17,14 +17,20 @@ class FirstScreen(Screen):
         page = wikipedia.page(query)
         image_link = page.images[0]
 
+        return image_link
+
+    def download_image(self):
         # Download the image
-        req = requests.get(image_link)
-        image_path = f"files/{query}.jpg"
+        req = requests.get(self.get_image_link())
+        image_path = f"files/image.jpg"
         with open(image_path, "wb") as file:
             file.write(req.content)
 
+        return image_path
+
+    def set_image(self):
         # Set the image in the widget
-        self.manager.current_screen.ids.img.source = image_path
+        self.manager.current_screen.ids.img.source = self.download_image()
 
 
 class RootWidget(ScreenManager):
