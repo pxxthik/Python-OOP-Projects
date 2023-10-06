979177013dbe7e44c043eb0842f01f1f8ecc5de9
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-06 18:53:31 +0530

    improve the ui design of photo sharer app

--- Webcam_Photo_Sharer/main.py
+++ Webcam_Photo_Sharer/main.py
@@ -14,12 +14,14 @@ Builder.load_file("frontend.kv")
 class CameraScreen(Screen):
     def start(self):
         """Starts camera and changes button text"""
+        self.ids.camera.opacity = 1
         self.ids.camera.play = True
         self.ids.camera_button.text = "Stop Camera"
         self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
         """Stops camera and changes button text"""
+        self.ids.camera.opacity = 0
         self.ids.camera.play = False
         self.ids.camera_button.text = "Start Camera"
         self.ids.camera.texture = None
