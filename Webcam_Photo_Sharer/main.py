b44046610e90ab84aab43a4e7497c46821f00808
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-06 18:06:09 +0530

    uploads and create sharable link in photo sharer app

--- Webcam_Photo_Sharer/main.py
+++ Webcam_Photo_Sharer/main.py
@@ -10,25 +10,36 @@ Builder.load_file("frontend.kv")
 
 class CameraScreen(Screen):
     def start(self):
+        """Starts camera and changes button text"""
         self.ids.camera.play = True
         self.ids.camera_button.text = "Stop Camera"
         self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
+        """Stops camera and changes button text"""
         self.ids.camera.play = False
         self.ids.camera_button.text = "Start Camera"
         self.ids.camera.texture = None
 
     def capture(self):
+        """Creates a filename with the current time and captures
+        and saves a photo image under that filename"""
         current_time = time.strftime('%Y%m%d-%H%M%S')
-        filepath = f"files/{current_time}.png"
-        self.ids.camera.export_to_png(filepath)
+        self.filepath = f"files/{current_time}.png"
+        self.ids.camera.export_to_png(self.filepath)
         self.manager.current = 'image_screen'
-        self.manager.current_screen.ids.img.source = filepath
+        self.manager.current_screen.ids.img.source = self.filepath
 
 
 class ImageScreen(Screen):
-    pass
+    def create_link(self):
+        """Access the photo filepath, uploads it to the web,
+        and inserts the linkin the Label widget"""
+        filepath = App.get_running_app().root.ids.camera_screen.filepath
+        file_sharer = FileSharer(filepath=filepath)
+        url = file_sharer.share()
+        self.ids.link.text = url
+
 
 
 class RootWidget(ScreenManager):
