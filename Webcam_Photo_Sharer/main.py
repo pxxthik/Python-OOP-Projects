98436207fb9fb5e4e87e0a92e6395a065b3f4acf
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-06 13:04:16 +0530

    add start and stop camera functionality to photo sharer app

--- Webcam_Photo_Sharer/main.py
+++ Webcam_Photo_Sharer/main.py
@@ -9,10 +9,14 @@ Builder.load_file("frontend.kv")
 
 class CameraScreen(Screen):
     def start(self):
-        pass
+        self.ids.camera.play = True
+        self.ids.camera_button.text = "Stop Camera"
+        self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
-        pass
+        self.ids.camera.play = False
+        self.ids.camera_button.text = "Start Camera"
+        self.ids.camera.texture = None
 
     def capture(self):
         pass
