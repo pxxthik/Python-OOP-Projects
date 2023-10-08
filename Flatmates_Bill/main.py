5f810edcbcadec8c870ec701513f1d8eebd18dc5
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-08 13:34:19 +0530

    complete home page setup in flatmates web version

--- Flatmates_Bill/main.py
+++ Flatmates_Bill/main.py
@@ -1,13 +1,20 @@
 from flask.views import MethodView
 from wtforms import Form
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 
 class HomePage(MethodView):
-    pass
+
+    def get(self):
+        return render_template('index.html')
 
 
 class BillFormPage(MethodView):
-    pass
+
+    def get(self):
+        return "Iam bill form page"
 
 
 class ResultsPage(MethodView):
@@ -16,3 +23,9 @@ class ResultsPage(MethodView):
 
 class BillForm(Form):
     pass
+
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
+
+app.run(debug=True)
