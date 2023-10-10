faaf5d0bbdf2d6d0a1539fd7ed2dc44a0ad97755
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-10 19:02:41 +0530

    implementing calorie temperature class in calorie web app

--- Calorie_Webapp/calorie.py
+++ Calorie_Webapp/calorie.py
@@ -1,3 +1,6 @@
+from temperature import Temperature
+
+
 class Calorie:
     """Represent amount of calories calculated with
     BMR = 10*weight + 6.25*height - 5*age - 10*temperature
@@ -10,4 +13,11 @@ class Calorie:
         self.weight = weight
 
     def calculate(self):
-        pass
+        result = 10 * self.weight + 6.5 * self.height + 5*self.age - self.temperature * 10
+        return result
+
+
+if __name__ == "__main__":
+    temperature = Temperature(city='rome', country='italy').get()
+    calorie = Calorie(temperature=temperature, weight=70, height=175, age=32)
+    print(calorie.calculate())
--- Calorie_Webapp/temperature.py
+++ Calorie_Webapp/temperature.py
@@ -2,10 +2,17 @@ class Temperature:
     """
     Represents a temperature value extracted from the timeanddate.com/weather webpage.
     """
+
     def __init__(self, country, city):
         self.city = city
         self.country = country
 
     def get(self):
-        pass
-        
+        # code for web scraping
+
+        return float(18)
+
+
+if __name__ == "__main__":
+    temperature = Temperature(country='usa', city='san francisco')
+    print(temperature.get())
