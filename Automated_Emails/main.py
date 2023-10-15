5ba8ca9065e8c3685b8d29a46127f9ab4da1a467
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-15 13:03:44 +0530

    sending batch emails

--- Automated_Emails/main.py
+++ Automated_Emails/main.py
@@ -1,7 +1,15 @@
-class NewsFeed:
+import yagmail
+import pandas
+from news import NewsFeed
 
-    def __init__(self, data):
-        self.data = data
+df = pandas.read_excel("people.xlsx")
 
-    def get(self):
-        pass
+for index, row in df.iterrows():
+    news_feed = NewsFeed(interest=row['interest'],
+                         from_date="2023-10-14",
+                         to_date="2023-10-15")
+    email = yagmail.SMTP(user="pythonprocourse1@gmail.com",
+                         password="python_pro_course_1")
+    email.send(to=row['email'],
+               subject=f"Your {row['interest']} news for today!",
+               contents=f"Hi {row['name']},\n See what's on about {row['interest']} today.\n{news_feed.get()}")
---
+++ Automated_Emails/news.py
@@ -0,0 +1,32 @@
+import requests
+
+
+class NewsFeed:
+    """Representing multiple news titles and links as a single string
+    """
+    base_url = "https://newsapi.org/v2/everything?"
+    api_key = "a9c14d7269894d69bc39c744037fa4ca"
+
+    def __init__(self, interest, from_date, to_date, language="en"):
+        self.language = language
+        self.to_date = to_date
+        self.from_date = from_date
+        self.interest = interest
+
+    def get(self):
+        url = f"{self.base_url}qInTitle={self.interest}&from={self.from_date}&to={self.to_date}&language={self.language}&apiKey={self.api_key}"
+
+        response = requests.get(url)
+        content = response.json()
+        articles = content['articles']
+
+        email_body = ""
+        for article in articles:
+            email_body += article['title'] + "\n" + article['url'] + "\n\n"
+
+        return email_body
+
+
+if __name__ == "__main__":
+    news_feed = NewsFeed(interest="nasa", from_date="2023-10-14", to_date="2023-10-15", language="en")
+    print(news_feed.get())
