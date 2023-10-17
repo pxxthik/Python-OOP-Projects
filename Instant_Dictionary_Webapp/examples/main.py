869335d0d2f70f4b999bcf43b21e6815e3465f22
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-17 18:10:30 +0530

    handle events

--- Instant_Dictionary_Webapp/examples/main.py
+++ Instant_Dictionary_Webapp/examples/main.py
@@ -6,23 +6,29 @@ def home():
     wp = jp.WebPage()
     div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
     div1 = jp.Div(a=div, classes="grid grid-cols-3 gap-4 p-4")
-    jp.Input(a=div1, placeholder="Enter first value",
-             classes="form-input")
-    jp.Input(a=div1, placeholder="Enter second value",
-             classes="form-input")
-    jp.Div(a=div1, text="Result goes here...",
-           classes="text-gray-600")
+    in_1 = jp.Input(a=div1, placeholder="Enter first value",
+                    classes="form-input")
+    in_2 = jp.Input(a=div1, placeholder="Enter second value",
+                    classes="form-input")
+    d_output = jp.Div(a=div1, text="Result goes here...",
+                      classes="text-gray-600")
     jp.Div(a=div1, text="Another div...",
            classes="text-gray-600")
     jp.Div(a=div1, text="Yet another div...",
            classes="text-gray-600")
 
     div2 = jp.Div(a=div, classes="grid grid-cols-2 gap-4")
-    jp.Button(a=div2, text="Calculate",
+    jp.Button(a=div2, text="Calculate", click=sum_up, in_1=in_1, in_2=in_2,
+              d=d_output,
               classes="border border-blue-500 m-2 py-1 px-4 rounded "
                       "text-blue-600 hover:bg-red-500 hover:text-white")
     jp.Div(a=div2, text="Im a cool interactive div!")
     return wp
+
+
+def sum_up(widget, msg):
+    sum = float(widget.in_1.value) + float(widget.in_2.value)
+    widget.d.text = sum
 
 
 # jp.Route("/", home)
