4d06b19a2d0e031cdd1bf86d6a55eb2bae142a5b
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-17 17:57:33 +0530

    put components in a grid

--- Instant_Dictionary_Webapp/examples/main.py
+++ Instant_Dictionary_Webapp/examples/main.py
@@ -5,15 +5,23 @@ import justpy as jp
 def home():
     wp = jp.WebPage()
     div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
-    jp.Input(a=div, placeholder="Enter first value",
+    div1 = jp.Div(a=div, classes="grid grid-cols-3 gap-4 p-4")
+    jp.Input(a=div1, placeholder="Enter first value",
              classes="form-input")
-    jp.Input(a=div, placeholder="Enter second value",
+    jp.Input(a=div1, placeholder="Enter second value",
              classes="form-input")
-    jp.Div(a=div, text="Result goes here...",
+    jp.Div(a=div1, text="Result goes here...",
            classes="text-gray-600")
-    jp.Button(a=div, text="Calculate",
+    jp.Div(a=div1, text="Another div...",
+           classes="text-gray-600")
+    jp.Div(a=div1, text="Yet another div...",
+           classes="text-gray-600")
+
+    div2 = jp.Div(a=div, classes="grid grid-cols-2 gap-4")
+    jp.Button(a=div2, text="Calculate",
               classes="border border-blue-500 m-2 py-1 px-4 rounded "
                       "text-blue-600 hover:bg-red-500 hover:text-white")
+    jp.Div(a=div2, text="Im a cool interactive div!")
     return wp
 
 
