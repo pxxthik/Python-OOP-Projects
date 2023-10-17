731e713aff31d0bf3a03b26b66e37edb3ad31d33
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-17 17:36:18 +0530

    add more components to the webpage

--- Instant_Dictionary_Webapp/examples/main.py
+++ Instant_Dictionary_Webapp/examples/main.py
@@ -4,17 +4,16 @@ import justpy as jp
 @jp.SetRoute("/")
 def home():
     wp = jp.WebPage()
-    jp.Div(a=wp, text="Hello world!",
-           classes="text-green-900 bg-yellow-400 font-serif text-lg")
-    jp.Div(a=wp, text="Hello again")
-    return wp
-
-
-@jp.SetRoute("/about")
-def about():
-    wp = jp.WebPage()
-    jp.Div(a=wp, text="Hi world!")
-    jp.Div(a=wp, text="Hi again")
+    div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+    jp.Input(a=div, placeholder="Enter first value",
+             classes="form-input")
+    jp.Input(a=div, placeholder="Enter second value",
+             classes="form-input")
+    jp.Div(a=div, text="Result goes here...",
+           classes="text-gray-600")
+    jp.Button(a=div, text="Calculate",
+              classes="border border-blue-500 m-2 py-1 px-4 rounded "
+                      "text-blue-600 hover:bg-red-500 hover:text-white")
     return wp
 
 
