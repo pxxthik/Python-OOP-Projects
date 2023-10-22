0ffb31df289ce7b7d43c71ae9f8ac5eb2b686177
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-22 15:45:32 +0530

    getting defination in output box

--- Instant_Dictionary_Webapp/webapp/dictionary.py
+++ Instant_Dictionary_Webapp/webapp/dictionary.py
@@ -1,4 +1,5 @@
 import justpy as jp
+import definition
 
 
 class Dictionary:
@@ -13,20 +14,19 @@ class Dictionary:
                classes="text-lg")
 
         input_div = jp.Div(a=div, classes="grid grid-cols-2")
-        jp.Input(a=input_div, placeholder="Type in a word here...",
-                 classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
-                         "focus:bg-white focus:outline-none focus:border-purple-500 py-2 px-4")
+        input_box = jp.Input(a=input_div, placeholder="Type in a word here...",
+                             classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
+                                     "focus:bg-white focus:outline-none focus:border-purple-500 py-2 px-4")
 
         output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
 
         jp.Button(a=input_div, text="Get Definition",
                   classes="border-2 text-gray-500", click=cls.get_definition,
-                  output_div=output_div)
-
+                  output_div=output_div, input_box=input_box)
 
         return wp
 
     @staticmethod
     def get_definition(widget, msg):
-        widget.output_div.text = "Something was displayed"
-
+        defined = definition.Definition(widget.input_box.value).get()
+        widget.output_div.text = " ".join(defined)
