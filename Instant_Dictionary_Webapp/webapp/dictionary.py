ca15a68f145e77e46b3aebd77acec4863ccbded7
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-22 14:02:05 +0530

    getting output in output div

--- Instant_Dictionary_Webapp/webapp/dictionary.py
+++ Instant_Dictionary_Webapp/webapp/dictionary.py
@@ -12,10 +12,21 @@ class Dictionary:
         jp.Div(a=div, text="Get the definition of any English word instantly as you type.",
                classes="text-lg")
 
-        jp.Input(a=div, placeholder="Type in a word here...",
+        input_div = jp.Div(a=div, classes="grid grid-cols-2")
+        jp.Input(a=input_div, placeholder="Type in a word here...",
                  classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
                          "focus:bg-white focus:outline-none focus:border-purple-500 py-2 px-4")
 
-        jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+
+        jp.Button(a=input_div, text="Get Definition",
+                  classes="border-2 text-gray-500", click=cls.get_definition,
+                  output_div=output_div)
+
 
         return wp
+
+    @staticmethod
+    def get_definition(widget, msg):
+        widget.output_div.text = "Something was displayed"
+
