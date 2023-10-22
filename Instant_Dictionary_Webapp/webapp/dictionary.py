7a2b898726eb10395cefd4fd8862cd6ebe0cf1fc
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-22 13:34:54 +0530

    time bomb in code

--- Instant_Dictionary_Webapp/webapp/dictionary.py
+++ Instant_Dictionary_Webapp/webapp/dictionary.py
@@ -4,7 +4,8 @@ import justpy as jp
 class Dictionary:
     path = "/dictionary"
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
         div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
         jp.Div(a=div, text="Instant English Dictionary!", classes="text-4xl m-2")
