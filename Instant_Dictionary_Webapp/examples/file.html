159360ff086434a21710c5e530bdcac118d84390
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-26 19:24:01 +0530

    making the layout

---
+++ Instant_Dictionary_Webapp/examples/file.html
@@ -0,0 +1,43 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
--- Instant_Dictionary_Webapp/webapp/home.py
+++ Instant_Dictionary_Webapp/webapp/home.py
@@ -4,12 +4,35 @@ import justpy as jp
 class Home:
     path = "/"
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_model="left",
+                            bordered=True)
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu",
+                click=cls.move_drawer, drawer=drawer)
+
+        jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")
+
+        container = jp.QPageContainer(a=layout)
+
+        div = jp.Div(a=container, classes="bg-gray-200 h-screen p-2")
         jp.Div(a=div, text="This is the Home page!", classes="text-4xl m-2")
         jp.Div(a=div, text="""
             long text
         """, classes="text-lg")
 
         return wp
+
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
+
