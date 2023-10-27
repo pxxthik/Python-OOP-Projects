1e21a978d20bc0328ecd6364f0f175bd6501473e
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-10-27 21:49:24 +0530

    making the routes automatic

--- Instant_Dictionary_Webapp/main.py
+++ Instant_Dictionary_Webapp/main.py
@@ -4,8 +4,14 @@ from webapp.home import Home
 from webapp.about import About
 from webapp.dictionary import Dictionary
 
-jp.Route(Home.path, Home.serve)
-jp.Route(About.path, About.serve)
-jp.Route(Dictionary.path, Dictionary.serve)
+imports = list(globals().values())
+
+for obj in imports:
+    if hasattr(obj, 'path'):
+        jp.Route(obj.path, obj.serve)
+
+# jp.Route(Home.path, Home.serve)
+# jp.Route(About.path, About.serve)
+# jp.Route(Dictionary.path, Dictionary.serve)
 
 jp.justpy(port=8001)
